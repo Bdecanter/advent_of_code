378753c4964077d6c231d6b6f53d08c9a0e1d2d7
Epock <benoit@myutopie.fr>
2025-12-07 09:30:48 +0100

    add day1 part 1

---
+++ src/_2025/day/day_1.rs
@@ -0,0 +1,86 @@
+pub fn part1(data: &str) -> i32 {
+    println!("Starting decode part one");
+
+    // we start at 50
+    let mut state: i32 = 50;
+    let mut nb_of_zero: i32 = 0;
+
+    data.lines().enumerate().for_each(|(y, line)| {
+        let l = line[1..].parse::<i32>().unwrap();
+
+        if line.chars().nth(0).unwrap() == 'L' {
+            state = state - l;
+
+        } else {
+            state = state + l;
+        }
+
+        while state < 0 || state > 99 {
+            if state > 99 {
+                state = state - 100;
+            }
+
+            if state < 0 {
+                state = state + 100;
+            }
+
+            if state < 99 && state > 0 {
+                continue
+            }
+        }
+
+        if state == 0 {
+            nb_of_zero = nb_of_zero + 1;
+        }
+    });
+    nb_of_zero
+}
+
+pub fn part2(data: &str) -> i32 {
+    println!("Starting decode part two");
+
+    // we start at 50
+    let mut state: i32 = 50;
+    let mut nb_of_zero: i32 = 0;
+
+    data.lines().enumerate().for_each(|(y, line)| {
+        let l = line[1..].parse::<i32>().unwrap();
+        println!("Line {}: State {} - {}", line, state, l);
+        if line.chars().nth(0).unwrap() == 'L' {
+            state = state - l;
+
+        } else {
+            state = state + l;
+        }
+
+        while state < 0 || state > 99 {
+
+            if state == 100 {
+                state = 0;
+            }
+
+            if state > 99 {
+                state = state - 100;
+                println!("ok un tour");
+                nb_of_zero = nb_of_zero + 1;
+            }
+
+            if state < 0 {
+                state = state + 100;
+                println!("ok un tour");
+                nb_of_zero = nb_of_zero + 1;
+            }
+
+            // if state < 99 && state > 0 {
+            //     continue
+            // }
+        }
+
+        if state == 0 {
+            nb_of_zero = nb_of_zero + 1;
+            println!("ZERO");
+        }
+    });
+    nb_of_zero
+
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod helpers;
+mod _2025;
+
+fn main() {
+    println!("--> Result: {}", _2025::day::day_1::part1(_2025::data::day_one::DAY_1));
+    println!("--> Result: {}", _2025::day::day_1::part2(_2025::data::day_one::EXAMPLE));
+}
+
