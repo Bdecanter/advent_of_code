74a230f2ff75d242d8885ff932f1fb31033db900
TheBen <benoit@kinomap.net>
2025-12-09 21:33:04 +0100

    save

--- src/_2025/day/day_3.rs
+++ src/_2025/day/day_3.rs
@@ -28,3 +28,67 @@ pub fn part1(data: &str) -> i32 {
 
     total
 }
+
+pub fn part2(data: &str) -> i32 {
+    let mut total :i32 = 0;
+
+    for (_line_idx, line) in data.lines().enumerate() {
+        let max_digit :i32 = 12;
+        let mut max_value: i32 = 0;
+        let mut max_value_tab: Vec<i32> = vec!();
+        let mut max_char_idx: usize = 0;
+
+        // we get the max value but keeping at least 12 digit
+        line.chars().enumerate().for_each(|(char_idx, char)| {
+            let val = char.to_digit(10).unwrap() as i32;
+
+            if max_value < val && char_idx < line.len() - max_digit as usize {
+                max_value = val;
+                max_char_idx = char_idx;
+            }
+        });
+
+        let rest = &line[max_char_idx + 1..];
+
+        println!("idx:{} -> {}", max_char_idx, max_value);
+        println!("Reste: {}", rest);
+
+        let mut next :i32 = 0;
+        let mut next_idx :usize = 0;
+        let mut digit_left :i32 = max_digit - 1;
+
+        for i in (0..max_digit - 1).rev() {
+            println!("{} -> {}", i, digit_left);
+
+            rest.chars().enumerate().for_each(|(char_idx, char)| {
+                let val :i32 = char.to_digit(10).unwrap() as i32;
+
+                if val > next && char_idx < line.len() - digit_left as usize {
+                    next = val;
+                    next_idx = char_idx;
+                }
+            })
+
+
+
+
+
+/*
+            if digit_left <= i {
+                println!("++++ digit left {}", digit_left);
+                break;
+            }
+            rest.chars().enumerate().for_each(|(char_idx, char)| {
+                let val :i32 = char.to_digit(10).unwrap() as i32;
+
+                if val > next && char_idx < line.len() - digit_left as usize {
+                    next = val;
+                    next_idx = char_idx;
+                }
+            })
+*/
+        }
+
+    }
+1
+}
